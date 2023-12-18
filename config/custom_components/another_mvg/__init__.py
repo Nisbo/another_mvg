2abfc7447937eeef21d800bed7ccab76c1949e7a
Nisbo <webmaster@0815co.de>
2023-12-18 16:48:07 +0100

    Create __init__.py

---
+++ config/custom_components/another_mvg/__init__.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
+
+# This file is not used at the moment, because the integration works direct with the sensor
+# The domain of your component. Should be equal to the name of your component.
+DOMAIN = "another_mvg"
+
+def setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up a skeleton component."""
+    # States are in the format DOMAIN.OBJECT_ID.
+    # hass.states.set('another_mvg.connections', 'Not used at the moment')
+
+    # Return boolean to indicate that initialization was successfully.
+    return True
