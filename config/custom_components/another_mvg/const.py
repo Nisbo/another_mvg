03ac9516790df289ade9da03426ea55d57e08115
Nisbo <webmaster@0815co.de>
2023-12-18 16:43:46 +0100

    Update const.py

--- config/custom_components/another_mvg/const.py
+++ config/custom_components/another_mvg/const.py
@@ -1,1 +1,12 @@
+from datetime import timedelta
 
+DOMAIN = "another_mvg"                           # name of the integration, dont change
+SCAN_INTERVAL = timedelta(seconds=60)            # updateinterval in seconds
+
+CONF_GLOBALID            = "globalid"            # required
+CONF_GLOBALID2           = "globalid2"           # optional but not recommened because of 2 API calls
+CONF_ONLYLINE            = "onlyline"            # optional
+CONF_LIMIT               = "limit"               # optional --> max 80
+CONF_HIDEDESTINATION     = "hidedestination"     # optional
+CONF_DOUBLESTATIONNUMBER = "doublestationnumber" # optional --> any String, if you want the globalid more than 1 times
+CONF_TRANSPORTTYPES      = "transporttypes"      # SBAHN,UBAHN,TRAM,BUS,REGIONAL_BUS (SCHIFF - There is a parameter in the MVG API, but dont know if it will return data, at the moment not supported)
