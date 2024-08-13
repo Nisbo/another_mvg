db7daefa20682e585a618b49c6da1278fb6d880d
Nisbo <webmaster@0815co.de>
2024-08-13 12:05:11 +0200

    Update __init__.py

--- custom_components/another_mvg/__init__.py
+++ custom_components/another_mvg/__init__.py
@@ -19,3 +19,21 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     
     # Return boolean to indicate that initialization was successfully.
     return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up Another MVG from a config entry."""
+    hass.data.setdefault(DOMAIN, {})
+    
+    # Den Sensor initialisieren
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
+
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    hass.data[DOMAIN].pop(entry.entry_id)
+
+    return True
