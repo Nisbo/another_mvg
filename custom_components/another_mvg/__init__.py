b30303e4301e27b3db432f34bc9a7d5ac0f33e32
Nisbo <webmaster@0815co.de>
2024-10-22 11:01:51 +0200

    Update __init__.py

--- custom_components/another_mvg/__init__.py
+++ custom_components/another_mvg/__init__.py
@@ -4,11 +4,9 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
 from .frontend import AnotherMvgCardRegistration
 
-# This file is not used at the moment, because the integration works direct with the sensor
 # The domain of your component. Should be equal to the name of your component.
 from .const import DOMAIN
 
-# def setup(hass: HomeAssistant, config: ConfigType) -> bool:
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up a skeleton component."""
     # States are in the format DOMAIN.OBJECT_ID.
