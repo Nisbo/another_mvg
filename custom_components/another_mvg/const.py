4600176a4d735ac47b9df5c8f49f9638a37b1293
Nisbo <webmaster@0815co.de>
2026-07-05 16:16:42 +0200

    Update version numbers for MVG cards

--- custom_components/another_mvg/const.py
+++ custom_components/another_mvg/const.py
@@ -56,7 +56,7 @@ ANOTHER_MVG_CARDS = [
     {
         "name": "Another MVG Card",
         "filename": "content-card-another-mvg.js",
-        "version": "3.0.0-BETA-2",
+        "version": "3.0.0-BETA-1",
     },
     {
         "name": "Another MVG Big Card",
@@ -66,6 +66,6 @@ ANOTHER_MVG_CARDS = [
     {
         "name": "Another MVG LiveMap Card",
         "filename": "content-card-another-mvg-livemap.js",
-        "version": "2.2.0-BETA-6",
+        "version": "3.0.0-BETA-3",
     },
 ]
